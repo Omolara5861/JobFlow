296ff8ae7b46d242752e9eda5de8354f2be513ee
Omolara Adebowale <debbiegterra@gmail.com>
2023-03-17 18:27:34 +0100

    feat: :passport_control: Add auth controller

---
+++ controllers/auth.js
@@ -0,0 +1,12 @@
+const register = async (req, res) => {
+    res.status(201).send('User Registered');
+}
+
+const login = async (req, res) => {
+    res.status(200).send('User Logged In')
+}
+
+modules.exports = {
+    register,
+    login
+}
