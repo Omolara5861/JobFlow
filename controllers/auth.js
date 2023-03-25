6ae329e3d576878d4e6583dd114bffce20d9055a
Omolara Adebowale <debbiegterra@gmail.com>
2023-03-25 13:33:03 +0100

    feat: :necktie: Add logic for the login functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
 const User = require('../models/user');
 const {StatusCodes} = require('http-status-codes');
+const BadRequestError = require('../errors');
+const UnauthenticatedError = require('../errors')
 
 const register = async (req, res) => {
     const user = await User.create({...req.body});
@@ -8,7 +10,14 @@ const register = async (req, res) => {
 }
 
 const login = async (req, res) => {
-    res.status(200).send('User Logged In')
+    const {email, password} = req.body;
+    if(!email || !password) throw new BadRequestError('Pls provide your email and password');
+    const user = await User.findOne({email});
+    if(!user) throw new UnauthenticatedError('Invalid Email Credential');
+    const isPasswordCorrect = User.comparePassword(password);
+    if(!isPasswordCorrect) throw new UnauthenticatedError('Invalid Password Credential');
+    const token = User.createJWT();
+    res.status(StatusCodes.OK).json({user: {name: user.name}, token});
 }
 
 module.exports = {
