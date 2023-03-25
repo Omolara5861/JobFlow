16e31ed74bd0185ad12543a60b6fe40cac49da26
Omolara5861 <debbiegterra@gmail.com>
2023-03-25 19:26:33 +0100

    refactor: :recycle: Refactor create method

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -12,7 +12,7 @@ const getJob = async (req, res) => {
 
 const createJob = async (req, res) => {
     req.body.createdBy = req.user.userID;
-    const job = await Job.create({...req.body});
+    const job = await Job.create(req.body);
     res.status(StatusCodes.CREATED).json({job});
 }
 
