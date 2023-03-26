64e7329b04c3170ad1c68d670d7e6c85e5828e78
Omolara5861 <debbiegterra@gmail.com>
2023-03-26 14:56:47 +0100

    chore: :lock: Add and setup security packages

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+//Security Packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 const authRoute = require('./routes/auth');
 const jobsRoute = require('./routes/jobs');
 
@@ -14,8 +20,16 @@ const connectDB = require('./db/connect');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000, //15 minutes
+  max: 100 //Limit each IP address to a maximum of 100 per windowMs
+}));
 app.use(express.json());
-// extra packages
+// security packages initialization
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
 
 // routes
 app.get('/', (req, res) => {
