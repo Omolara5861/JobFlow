7966bd06a5d54e61ec7b91d9a8e71d1acd743d42
Omolara5861 <debbiegterra@gmail.com>
2023-03-28 10:39:46 +0100

    fix: :bug: Fix authentication bug

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -9,7 +9,7 @@ const auth = async (req, res, next) => {
     const token = authHeaders.split(" ")[1];
     try {
         const payload = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = { userID: payload.id, name: payload.name };
+        req.user = { userID: payload.userID, name: payload.name };
         // const user = await User.findById(payload.id).select('-password');
         // req.user = user;
         next();
