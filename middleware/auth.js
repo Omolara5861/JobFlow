4491d37eeb1cf8d3f24cf21b47baa3f3df9e792a
Omolara5861 <debbiegterra@gmail.com>
2023-03-28 21:19:09 +0100

    chore: :fire: Remove comment file

--- middleware/auth.js
+++
@@ -1,6 +0,0 @@
-// This module exports a middleware function named "auth". 
-// This middleware function receives a request, a response and a next function as arguments. 
-// It checks if the request headers contain a valid "Bearer" token, throwing an error if it's not valid or not present.
-// Then, it decodes the token using the JWT_SECRET environment variable and sets a "userID" and "name" property in the "req.user" object.
-// Finally, it calls the "next" function to pass the control to the next middleware or endpoint.
-// Note that there's a commented code block, which suggests that the authentication could also involve a database query. However, this implementation doesn't use it.
