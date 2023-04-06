c2b36cdeef0b06a7383ab9efd5b2066464149d2e
Omolara5861 <debbiegterra@gmail.com>
2023-04-06 14:29:22 +0100

    fix: :bug: Fix backend link

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-axios.defaults.baseURL = 'http://http://localhost:4002/';
+axios.defaults.baseURL = 'http://localhost:4002/api/v1';
 
 axios.interceptors.request.use(function (req) {
   const user = localStorage.getItem('user');
